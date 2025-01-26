2f536a8cc5cb9b0d40e38ad1a72595538742ef72
Juunibyou <danijunn@gmail.com>
2025-01-25 18:58:49 -0600

    Updated

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,6 +3,6 @@ package org.example;
 public class App {
   public static void main(String[] args) {
      HelloWorld helloWorld = new HelloWorld();
-     System.out.printl(helloWorld.getGreeting());
+     System.out.println(helloWorld.getGreeting());
   }
 }
